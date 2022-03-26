c08bcfd5113c28151a83963ec9e76f0f101e732b
MF <Frotan1954@gamil.com>
2022-03-26 23:27:56 +0430

    this is my first commit

---
+++ src/Note.java
@@ -0,0 +1,18 @@
+public class Note {
+    public static void main(String[] args) {
+        /*
+          *git init
+        : Done only once for each project.This is used for initilizing the project. This will create .git forlder to store the project versions!!!
+        *git add .
+        : Done after each code change.
+        *git commit -m "first commit"
+        : Done after each code change
+        *git branch -M main
+        :Done once for each project
+        *git remote add origin https://github.com/abayramus/my-first-git-repo.git
+        :Done once for each project
+        *git push -u origin main
+        :Done once for each project
+         */
+    }
+}
