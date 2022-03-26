5c3a65acb7f293e96caa89fe28ad68983cc6a208
MF <Frotan1954@gamil.com>
2022-03-27 00:25:03 +0430

    This is My last Update

--- src/GitHub01.java
+++ src/GitHub01.java
@@ -1,5 +1,6 @@
 public class GitHub01 {
     public static void main(String[] args) {
         System.out.println("This is My First github");
+        System.out.println("i Finished The GitHub");
     }
 }
