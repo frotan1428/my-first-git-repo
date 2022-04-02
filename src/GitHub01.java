7c88503fcdd722dda55da553d95733ad174d788e
MF <Frotan1954@gamil.com>
2022-04-02 18:53:57 +0430

    This is day02 Example

--- src/GitHub01.java
+++ src/GitHub01.java
@@ -2,5 +2,6 @@ public class GitHub01 {
     public static void main(String[] args) {
         System.out.println("This is My First github");
         System.out.println("i Finished The GitHub");
+        System.out.println("This is another class");
     }
 }
